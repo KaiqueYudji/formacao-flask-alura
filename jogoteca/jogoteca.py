321c1f75d57b98b77ee36af87c1509de69c2e08d
KaiqueYudji <yudjikaique@gmail.com>
2024-05-31 16:56:27 -0300

    Melhorando o codigo e a usabilidade

--- jogoteca/jogoteca.py
+++ jogoteca/jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect # Estas são funções helpers, que ajudam o flask a solucionar proclemas que ele sozinho não consegue
 
 class Game:
     def __init__(self, name, category, console):
@@ -21,7 +21,7 @@ games_array = [game1, game2, game3]
 application = Flask(__name__)
 
 
-@application.route('/inicio')
+@application.route('/')
 def get_games():
     return render_template('lista.html', titulo='Jogos', jogos=games_array)#O flask tem uma padronização por "natureza", na qual ele sabe que os aqruivos HTML estão na pasta templates. Por causa disso, não preciso colocar o path do arquivo HTML, apenas o seu nome.
 
@@ -33,17 +33,18 @@ def new():
 
 
 
-@application.route('/criar')
+@application.route('/criar', methods=['POST',])
 def create_game():
-    name = request.form['nome']
+    name = request.form['nome']#Este request que importamos na parte superior do script é responsável por capturar as informações dos formulários. O valor passado entre colchetes é o atributo do elemento HTML que ele quer pegar o conteudo 
     category = request.form['categoria']
     console = request.form['console']
 
     game = Game(name, category, console)
     games_array.append(game)
-    return render_template('lista.html', titulo='Jogos', jogos=games_array)
+    return redirect('/')
 
 
+application.run(debug=True)
 
 
 application.run()
